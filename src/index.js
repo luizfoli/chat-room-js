6de8f5b4fce544ac80b905adb1b483c9137fee63
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-07-28 08:22:18 -0300

    feat: creating logic to save user id

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-let userIndex = 0;
 
 io.on('connection', (socket) => {
 
-    const thisUser = userIndex;
-    userIndex++;
+    const user = createUser(socket.id);
+    console.log(`User with id ${user.id} connected`);
 
-
-    console.log(`a user ${thisUser} connected`);
     socket.on('disconnect', () => {
-      console.log(`user ${thisUser} disconnected`);
+        console.log(`User with id ${user.id} disconnected`);
     });
-  });
+});
 
 
 http.listen(PORT, () => {
     console.log(`Server it's running in port ${PORT}`);
 });   
+
+function createUser(id) {
+    return {
+        id
+    };
+}
